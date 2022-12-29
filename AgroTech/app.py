92726351e1981233e0d7bb310bf85c656c1f63c5
Santhosh Raminedi <87355988+Santhosh2231@users.noreply.github.com>
2022-12-29 22:10:43 +0530

    deployment v1

--- AgroTech/app.py
+++ AgroTech/app.py
@@ -2,13 +2,17 @@
 from flask import Flask
 from flask_restful import Api
 from routes.routes import initialize_routes
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='react-app/build')
 CORS(app)
 
+@app.route("/")
+@cross_origin
+def serve():
+    return send_from_directory(app.static_folder,'index.html')
 api = Api(app)
 initialize_routes(api)
 
 if __name__=="__main__":
-    app.run(debug=True,port=8000)
+    app.run()
